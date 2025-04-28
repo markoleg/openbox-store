90e0172edca9cb236452803221b112438d568d70
markoleg <94861029+markoleg@users.noreply.github.com>
2025-04-28 17:20:01 +0300

    fixed login greeting & new icons

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,7 +3,7 @@ import styles from '@/components/LoginForm/LoginForm.module.css';
 export default function LoginPage() {
   return (
     <div className={styles.loginPage}>
-      <h1>Login</h1>
+      <h1>Welcome, ZheZhe!</h1>
       <LoginForm />
     </div>
   );
